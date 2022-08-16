051c910a9d1f6c574a4b6687d185a8a5461fa062
JiIn Kim <rlawldls11@naver.com>
2022-08-16 16:48:33 +0900

    글작성 구조 변경

--- src/main/java/com/noticeboardproject/controller/write/WriteController.java
+++ src/main/java/com/noticeboardproject/controller/write/WriteController.java
@@ -3,7 +3,6 @@ package com.noticeboardproject.controller.write;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.noticeboardproject.entity.TableList;
 import com.noticeboardproject.service.write.WriteService;
@@ -33,6 +33,17 @@ public class WriteController extends HttpServlet{
 		tableList.setWriter_id((String)request.getSession().getAttribute("userId"));
 		writeService.write(tableList);
 		return "redirect:/noticeboard/writemove";
+	}
+	
+	@RequestMapping("/noticeboard/writemove")
+	public void movePage(HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<script>");
+		out.println("alert('글이 생성되었습니다.')");
+		out.println("location.href='/noticeboard/list'");
+		out.println("</script>");
+		
+		
 	}
 
 	
