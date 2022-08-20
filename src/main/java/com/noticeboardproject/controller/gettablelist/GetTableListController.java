89933134c6622ed286a08df2cda500a2bceb30ff
JiIn Kim <rlawldls11@naver.com>
2022-08-20 19:26:02 +0900

    페이징처리 1

--- src/main/java/com/noticeboardproject/controller/gettablelist/GetTableListController.java
+++ src/main/java/com/noticeboardproject/controller/gettablelist/GetTableListController.java
@@ -1,6 +1,7 @@
 package com.noticeboardproject.controller.gettablelist;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.noticeboardproject.entity.TableList;
 import com.noticeboardproject.service.table.TableService;
 
 @Controller
@@ -19,7 +21,15 @@ public class GetTableListController {
 	@GetMapping("/noticeboard/list")
 	public String getTable(Model model,
 			@PageableDefault(sort = "id", direction = Sort.Direction.DESC)Pageable pageable) {
-		model.addAttribute("tableList", tableService.getTableList(pageable));
+		Page<TableList> tableList = tableService.getTableList(pageable);
+		
+		int totalPage = tableList.getTotalPages()+1;
+		int nowPage = tableList.getPageable().getPageNumber();
+		System.out.println("totalPage : " + totalPage);
+		System.out.println("nowPage : " + nowPage);
+		model.addAttribute("nowPage", nowPage);
+		model.addAttribute("totalPage", totalPage);
+		model.addAttribute("tableList",tableList);
 		return "view/tablelist";
 	}
 	
