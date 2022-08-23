54c1788228383cc22b8804816d5a6dddcb5da46e
JiIn Kim <rlawldls11@naver.com>
2022-08-23 16:53:39 +0900

    조회수증가 오류수정

--- src/main/java/com/noticeboardproject/TableListRepository.java
+++ src/main/java/com/noticeboardproject/TableListRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.noticeboardproject.entity.TableList;
@@ -14,6 +15,6 @@ public interface TableListRepository extends JpaRepository<TableList,Integer>{
 	Page<TableList> findByTitleContaining(Pageable pageable, String searchKeyword);
 	
 	@Modifying
-	@Query("update TableList t set t.hit = t.hit + 1 where t.id = id")
-	int updateHit(Integer id);
+	@Query("update TableList t set t.hit = t.hit + 1 where t.id = :id")
+	int updateHit(@Param("id")Integer id);
 }
